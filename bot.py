22dbbedd52a4e6e94c02f33f6f44659417f8f9b6
Pierick Prost <prost.pierrick@outlook.fr>
2020-07-14 15:50:05 +0200

    playing only 30 seconds before disconnect when rickrolling

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 import discord
-import youtube_dl #TODO: Remove if unused
+import time
+
+# import youtube_dl #TODO: Remove if unused
 from config import *
 from discord.ext import commands
 
@@ -12,13 +14,6 @@ client = commands.Bot(command_prefix='>')
 
 async def debug(channel: discord.channel.TextChannel, message):
     await channel.send(message)
-
-async def join(ctx):
-    text_channel = ctx.message.channel
-    voip_channel = ctx.message.author.voice.channel
-    await voip_channel.connect()
-    await text_channel.send('Connected in ' + voip_channel.name)
-    return voip_channel
 
 
 @client.event
@@ -31,8 +26,8 @@ async def rick(ctx):
     voip_channel = ctx.message.author.voice.channel
     vc = await voip_channel.connect()
     vc.play(discord.FFmpegPCMAudio(source='rickroll.mp3'), after=lambda e: print('\x1b[35m------- Rickroll done -------\x1b[37m'))
-    if not vc.is_playing():
-        await vc.disconnect()
+    time.sleep(30)
+    await vc.disconnect()
 
 
 @client.command(pass_context=True)
