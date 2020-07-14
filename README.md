a4a40179db528e5b96d0fad5034185415b2374fd
Pierrick Prost <prost.pierrick@outlook.fr>
2020-07-14 14:11:16 +0200

    adding rickroll command

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# BlindtestBot
-Discord Blindtest Bot
+# BlindtestBot [ STILL IN DEVELOPPEMENT: 14/07/2020]
+Blindtest bot for discord using discord.py (not really made for big servers)
+
+# Dependencies
+The bot need python3, discord.py and PyNaCl to run, you can install them by using these commands:
+```
+sudo dnf install python3 (this line may change according to your Linux ditribution)
+python3 -m pip install pip discord.py pynacl
+```
--- bot.py
+++ bot.py
@@ -1,20 +1,44 @@
 #!/usr/bin/python3
 
 import discord
+import youtube_dl #TODO: Remove if unused
 from config import *
 from discord.ext import commands
 
+
 TOKEN = bot_token
-client = commands.Bot(command_prefix='@')
+client = commands.Bot(command_prefix='>')
+
+
+async def debug(channel: discord.channel.TextChannel, message):
+    await channel.send(message)
+
+async def join(ctx):
+    text_channel = ctx.message.channel
+    voip_channel = ctx.message.author.voice.channel
+    await voip_channel.connect()
+    await text_channel.send('Connected in ' + voip_channel.name)
+    return voip_channel
+
 
 @client.event
 async def on_ready():
-    print('\x1b[32m------------\n|Bot online|\n------------\x1b[37m')
+    print('\x1b[32m        ------------\n        |Bot online|\n        ------------\x1b[37m')
+
 
 @client.command(pass_context=True)
-async def join(ctx):
-    channel = ctx.message.author.voice.channel
-    await channel.connect()
+async def rick(ctx):
+    voip_channel = ctx.message.author.voice.channel
+    vc = await voip_channel.connect()
+    vc.play(discord.FFmpegPCMAudio(source='rickroll.mp3'), after=lambda e: print('\x1b[35m------- Rickroll done -------\x1b[37m'))
+    if not vc.is_playing():
+        await vc.disconnect()
+
+
+@client.command(pass_context=True)
+async def hello(ctx):
+    text_channel = ctx.message.channel
+    await text_channel.send('Hello')
 
 
 client.run(TOKEN)
